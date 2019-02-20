c567cbec68d8e18beecc4d8896794e8c280af33d
inchie_chen <eleanorbutemily@gmail.com>
2019-02-20 00:12:55 -0800

    Working ver.
    first commit

--- src/index.js
+++ src/index.js
@@ -6,50 +6,6 @@ import "./styles.css";
 import { Provider } from "react-redux";
 import store from "./redux/stores";
 
-// class App extends Component {
-//   constructor(props) {
-//     console.log(props);
-//     super(props);
-//   }
-//   render() {
-//     return (
-//       <div className="App">
-//         <h1>Hello CodeSandbox</h1>
-//         <h2>Start editing to see some magic happen!</h2>
-//         <MyList {...this.props} />
-//         <Recommendation {...this.props} />
-//         <div>
-//           <ul>
-//             {this.props.mylist.map(e => {
-//               return <li id={e.id}>e.title</li>;
-//             })}
-//           </ul>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-// const App1 = connect(
-//   state => {
-//     console.log(state, "state");
-//     return {
-//       recommendations: state.recommendations,
-//       mylist: state.mylist
-//     };
-//   },
-//   dispatch => {
-//     return {
-//       removeLike: id => {
-//         dispatch(actions.removeLike(id));
-//       },
-//       addLike: id => {
-//         dispatch(actions.addLike(id));
-//       }
-//     };
-//   }
-// )(App);
-
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <Provider store={store}>
